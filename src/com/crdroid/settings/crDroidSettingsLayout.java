RisingTechOSS/android_packages_apps_Personalizations

settings: Bring in the kitchen dashboard
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/crdroid/settings/crDroidSettingsLayout.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2022 crDroid Android Project
+ * Copyright (C) 2022 Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,203 +14,25 @@
  * limitations under the License.
  */
 
-package com.crdroid.settings;
+ package com.crdroid.settings;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
-
-import com.crdroid.settings.fragments.About;
-import com.crdroid.settings.fragments.Buttons;
-import com.crdroid.settings.fragments.LockScreen;
-import com.crdroid.settings.fragments.QuickSettings;
-import com.crdroid.settings.fragments.Miscellaneous;
-import com.crdroid.settings.fragments.Navigation;
-import com.crdroid.settings.fragments.Notifications;
-import com.crdroid.settings.fragments.Sound;
-import com.crdroid.settings.fragments.StatusBar;
-import com.crdroid.settings.fragments.UserInterface;
-
-public class crDroidSettingsLayout extends SettingsPreferenceFragment {
-
-    private static final String TAG = "crDroidSettingsLayout";
-    ViewPager mViewPager;
-    ViewGroup mContainer;
-    PagerSlidingTabStrip mTabs;
-    SectionsPagerAdapter mSectionsPagerAdapter;
-    protected Context mContext;
-
-    private static final int MENU_RESET = Menu.FIRST;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getActivity().setTitle(R.string.crdroid_settings_title);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mContainer = container;
-        View view = inflater.inflate(R.layout.crdroid_settings, container, false);
-        mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
-        mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.setClipChildren(true);
-        mViewPager.setClipToPadding(true);
-        mTabs.setViewPager(mViewPager);
-        mContext = getActivity().getApplicationContext();
-
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle saveState) {
-        super.onSaveInstanceState(saveState);
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.add(0, MENU_RESET, 0, R.string.reset_settings_title)
-                .setIcon(R.drawable.ic_reset)
-                .setAlphabeticShortcut('r')
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM |
-                        MenuItem.SHOW_AS_ACTION_WITH_TEXT);
-    }
-
-    public void resetAll(Context context) {
-        new ResetAllTask(context).execute();
-    }
-
-    public void showResetAllDialog(Context context) {
-        new AlertDialog.Builder(context)
-                .setTitle(R.string.reset_settings_title)
-                .setMessage(R.string.reset_settings_message)
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        resetAll(context);
-                    }
-                })
-                .setNegativeButton(R.string.cancel, null)
-                .show();
-    }
-
-    private class ResetAllTask extends AsyncTask<Void, Void, Void> {
-        private Context rContext;
-
-        public ResetAllTask(Context context) {
-            super();
-            rContext = context;
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            Buttons.reset(rContext);
-            LockScreen.reset(rContext);
-            QuickSettings.reset(rContext);
-            Miscellaneous.reset(rContext);
-            Navigation.reset(rContext);
-            Notifications.reset(rContext);
-            Sound.reset(rContext);
-            StatusBar.reset(rContext);
-            UserInterface.reset(rContext);
-            finish();
-            startActivity(getIntent());
-            return null;
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case MENU_RESET:
-                 showResetAllDialog(getActivity());
-                return true;
-            default:
-                return false;
-        }
-    }
-
-    class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        String titles[] = getTitles();
-        private Fragment frags[] = new Fragment[titles.length];
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-            frags[0] = new StatusBar();
-            frags[1] = new QuickSettings();
-            frags[2] = new LockScreen();
-            frags[3] = new Navigation();
-            frags[4] = new Buttons();
-            frags[5] = new UserInterface();
-            frags[6] = new Notifications();
-            frags[7] = new Sound();
-            frags[8] = new Miscellaneous();
-            frags[9] = new About();
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return frags[position];
-        }
-
-        @Override
-        public int getCount() {
-            return frags.length;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return titles[position];
-        }
-    }
-
-    private String[] getTitles() {
-        String titleString[];
-        titleString = new String[] {
-            getString(R.string.statusbar_title),
-            getString(R.string.quicksettings_title),
-            getString(R.string.lockscreen_title),
-            getString(R.string.navigation_title),
-            getString(R.string.button_title),
-            getString(R.string.ui_title),
-            getString(R.string.notifications_title),
-            getString(R.string.sound_title),
-            getString(R.string.misc_title),
-            getString(R.string.about_crdroid)
-        };
-        return titleString;
-    }
-
-    @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+ import android.os.Bundle;
+ import android.content.Context;
+ import com.android.settings.R;
+ import com.android.settings.SettingsPreferenceFragment;
+ import com.android.internal.logging.nano.MetricsProto;
+ 
+ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
+ 
+     @Override
+     public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+ 
+         addPreferencesFromResource(R.xml.rice_dashboard);
      }
-}
+ 
+     @Override
+     public int getMetricsCategory() {
+         return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+     }
+ } 

ProjectMatrixx/android_packages_apps_MatrixxSettings

src/com/crdroid/settings/crDroidSettingsLayout.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2022 crDroid Android Project
+ * Copyright (C) 2022 Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,203 +14,25 @@
  * limitations under the License.
  */
 
-package com.crdroid.settings;
+ package com.crdroid.settings;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
-
-import com.crdroid.settings.fragments.About;
-import com.crdroid.settings.fragments.Buttons;
-import com.crdroid.settings.fragments.LockScreen;
-import com.crdroid.settings.fragments.QuickSettings;
-import com.crdroid.settings.fragments.Miscellaneous;
-import com.crdroid.settings.fragments.Navigation;
-import com.crdroid.settings.fragments.Notifications;
-import com.crdroid.settings.fragments.Sound;
-import com.crdroid.settings.fragments.StatusBar;
-import com.crdroid.settings.fragments.UserInterface;
-
-public class crDroidSettingsLayout extends SettingsPreferenceFragment {
-
-    private static final String TAG = "crDroidSettingsLayout";
-    ViewPager mViewPager;
-    ViewGroup mContainer;
-    PagerSlidingTabStrip mTabs;
-    SectionsPagerAdapter mSectionsPagerAdapter;
-    protected Context mContext;
-
-    private static final int MENU_RESET = Menu.FIRST;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getActivity().setTitle(R.string.crdroid_settings_title);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mContainer = container;
-        View view = inflater.inflate(R.layout.crdroid_settings, container, false);
-        mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
-        mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.setClipChildren(true);
-        mViewPager.setClipToPadding(true);
-        mTabs.setViewPager(mViewPager);
-        mContext = getActivity().getApplicationContext();
-
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle saveState) {
-        super.onSaveInstanceState(saveState);
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.add(0, MENU_RESET, 0, R.string.reset_settings_title)
-                .setIcon(R.drawable.ic_reset)
-                .setAlphabeticShortcut('r')
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM |
-                        MenuItem.SHOW_AS_ACTION_WITH_TEXT);
-    }
-
-    public void resetAll(Context context) {
-        new ResetAllTask(context).execute();
-    }
-
-    public void showResetAllDialog(Context context) {
-        new AlertDialog.Builder(context)
-                .setTitle(R.string.reset_settings_title)
-                .setMessage(R.string.reset_settings_message)
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        resetAll(context);
-                    }
-                })
-                .setNegativeButton(R.string.cancel, null)
-                .show();
-    }
-
-    private class ResetAllTask extends AsyncTask<Void, Void, Void> {
-        private Context rContext;
-
-        public ResetAllTask(Context context) {
-            super();
-            rContext = context;
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            Buttons.reset(rContext);
-            LockScreen.reset(rContext);
-            QuickSettings.reset(rContext);
-            Miscellaneous.reset(rContext);
-            Navigation.reset(rContext);
-            Notifications.reset(rContext);
-            Sound.reset(rContext);
-            StatusBar.reset(rContext);
-            UserInterface.reset(rContext);
-            finish();
-            startActivity(getIntent());
-            return null;
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case MENU_RESET:
-                 showResetAllDialog(getActivity());
-                return true;
-            default:
-                return false;
-        }
-    }
-
-    class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        String titles[] = getTitles();
-        private Fragment frags[] = new Fragment[titles.length];
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-            frags[0] = new StatusBar();
-            frags[1] = new QuickSettings();
-            frags[2] = new LockScreen();
-            frags[3] = new Navigation();
-            frags[4] = new Buttons();
-            frags[5] = new UserInterface();
-            frags[6] = new Notifications();
-            frags[7] = new Sound();
-            frags[8] = new Miscellaneous();
-            frags[9] = new About();
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return frags[position];
-        }
-
-        @Override
-        public int getCount() {
-            return frags.length;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return titles[position];
-        }
-    }
-
-    private String[] getTitles() {
-        String titleString[];
-        titleString = new String[] {
-            getString(R.string.statusbar_title),
-            getString(R.string.quicksettings_title),
-            getString(R.string.lockscreen_title),
-            getString(R.string.navigation_title),
-            getString(R.string.button_title),
-            getString(R.string.ui_title),
-            getString(R.string.notifications_title),
-            getString(R.string.sound_title),
-            getString(R.string.misc_title),
-            getString(R.string.about_crdroid)
-        };
-        return titleString;
-    }
-
-    @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+ import android.os.Bundle;
+ import android.content.Context;
+ import com.android.settings.R;
+ import com.android.settings.SettingsPreferenceFragment;
+ import com.android.internal.logging.nano.MetricsProto;
+ 
+ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
+ 
+     @Override
+     public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+ 
+         addPreferencesFromResource(R.xml.matrixx_dashboard);
      }
-}
+ 
+     @Override
+     public int getMetricsCategory() {
+         return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+     }
+ } 