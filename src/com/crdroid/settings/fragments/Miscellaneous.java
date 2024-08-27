93f467c16de2f98864c0b4cda49dad7dba977ea4
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-27 11:18:26 +0530

    matrixx:Implement json-based game spoofing [2/2]
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -42,6 +42,9 @@ import com.android.settingslib.search.SearchIndexable;
 import com.crdroid.settings.fragments.misc.SensorBlock;
 
 import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.InputStream;
@@ -63,6 +66,8 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
     private static final String SYS_NETFLIX_SPOOF = "persist.sys.pixelprops.netflix";
     private static final String KEY_THREE_FINGERS_SWIPE = "three_fingers_swipe";
+    private static final String SYS_GAMEPROP_ENABLED = "persist.sys.gameprops.enabled";
+    private static final String KEY_GAME_PROPS_JSON_FILE_PREFERENCE = "game_props_json_file_preference";
     private static final String KEY_PIF_JSON_FILE_PREFERENCE = "pif_json_file_preference";
 
     private Preference mPropOptions;
@@ -71,6 +76,10 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
     private ListPreference mThreeFingersSwipeAction;
     
     private Preference mPifJsonFilePreference;
+
+    private Preference mGamePropsJsonFilePreference;
+    private Preference mGamePropsSpoof;
+
     private Handler mHandler;
 
     @Override
@@ -80,9 +89,13 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         mHandler = new Handler();
         addPreferencesFromResource(R.xml.crdroid_settings_misc);
 
+        mGamePropsSpoof = findPreference(SYS_GAMEPROP_ENABLED);
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
 
+        mPifJsonFilePreference = findPreference(KEY_PIF_JSON_FILE_PREFERENCE);
+        mGamePropsJsonFilePreference = findPreference(KEY_GAME_PROPS_JSON_FILE_PREFERENCE);    
+        mGamePropsSpoof.setOnPreferenceChangeListener(this);
         mPropOptions = (Preference) findPreference(SYS_PROP_OPTIONS);
         mPropOptions.setOnPreferenceChangeListener(this);
         mPocketJudge = (Preference) prefScreen.findPreference(POCKET_JUDGE);
@@ -90,7 +103,6 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
                 com.android.internal.R.bool.config_pocketModeSupported);
         if (!mPocketJudgeSupported)
             prefScreen.removePreference(mPocketJudge);
-        mPifJsonFilePreference = findPreference(KEY_PIF_JSON_FILE_PREFERENCE);
 
         Action defaultThreeFingersSwipeAction = Action.fromIntSafe(res.getInteger(
                 org.lineageos.platform.internal.R.integer.config_threeFingersSwipeBehavior));
@@ -123,44 +135,113 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
         if (preference == mPifJsonFilePreference) {
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("application/json");
-            startActivityForResult(intent, 10001);
+        mPifJsonFilePreference.setOnPreferenceClickListener(pref -> {
+            openFileSelector(10001);
             return true;
-        }
-        return super.onPreferenceTreeClick(preference);
+        });
+        return true;
+    } else if (preference == mGamePropsJsonFilePreference) {
+        mGamePropsJsonFilePreference.setOnPreferenceClickListener(pref -> {
+            openFileSelector(10002);
+            return true;
+        });
+        return true;
     }
+        return super.onPreferenceTreeClick(preference); // Default handling
+    }
+
+   private void openFileSelector(int requestCode) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("application/json");
+        startActivityForResult(intent, requestCode);
+   }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 10001 && resultCode == Activity.RESULT_OK) {
+        if (resultCode == Activity.RESULT_OK && data != null) {
             Uri uri = data.getData();
-            Log.d(TAG, "URI received: " + uri.toString());
-            try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
-                if (inputStream != null) {
-                    String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-                    Log.d(TAG, "JSON data: " + json);
-                    JSONObject jsonObject = new JSONObject(json);
-                    for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
-                        String key = it.next();
-                        String value = jsonObject.getString(key);
-                        Log.d(TAG, "Setting property: persist.sys.pihooks_" + key + " = " + value);
-                        SystemProperties.set("persist.sys.pihooks_" + key, value);
+            if (uri != null) {
+                if (requestCode == 10001) {
+                    loadPifJson(uri);
+                } else if (requestCode == 10002) {
+                    loadGameSpoofingJson(uri);
+                }
+            }
+        }
+    }
+
+    private void loadPifJson(Uri uri) {
+        Log.d(TAG, "Loading PIF JSON from URI: " + uri.toString());
+        try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
+            if (inputStream != null) {
+                String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+                Log.d(TAG, "PIF JSON data: " + json);
+                JSONObject jsonObject = new JSONObject(json);
+                for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+                    String key = it.next();
+                    String value = jsonObject.getString(key);
+                    Log.d(TAG, "Setting PIF property: persist.sys.pihooks_" + key + " = " + value);
+                    SystemProperties.set("persist.sys.pihooks_" + key, value);
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading PIF JSON or setting properties", e);
+        }
+        mHandler.postDelayed(() -> {
+            systemUtils.showSystemRestartDialog(getContext());
+        }, 1250);
+    }
+
+    private void loadGameSpoofingJson(Uri uri) {
+        Log.d(TAG, "Loading Game Props JSON from URI: " + uri.toString());
+        try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
+            if (inputStream != null) {
+                String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+                Log.d(TAG, "Game Props JSON data: " + json);
+                JSONObject jsonObject = new JSONObject(json);
+                for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+                    String key = it.next();
+                    if (key.startsWith("PACKAGES_") && !key.endsWith("_DEVICE")) {
+                        String deviceKey = key + "_DEVICE";
+                        if (jsonObject.has(deviceKey)) {
+                            JSONObject deviceProps = jsonObject.getJSONObject(deviceKey);
+                            JSONArray packages = jsonObject.getJSONArray(key);
+                            for (int i = 0; i < packages.length(); i++) {
+                                String packageName = packages.getString(i);
+                                Log.d(TAG, "Spoofing package: " + packageName);
+                                setGameProps(packageName, deviceProps);
+                            }
+                        }            
                     }
                 }
-            } catch (Exception e) {
-                Log.e(TAG, "Error reading JSON or setting properties", e);
             }
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading Game Props JSON or setting properties", e);
+        }
             mHandler.postDelayed(() -> {
                 systemUtils.showSystemRestartDialog(getContext());
             }, 1250);
+    }
+
+    private void setGameProps(String packageName, JSONObject deviceProps) {
+        try {
+            for (Iterator<String> it = deviceProps.keys(); it.hasNext(); ) {
+                String key = it.next();
+                String value = deviceProps.getString(key);
+                String systemPropertyKey = "persist.sys.gameprops." + packageName + "." + key;
+                SystemProperties.set(systemPropertyKey, value);
+                Log.d(TAG, "Set system property: " + systemPropertyKey + " = " + value);
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing device properties", e);
         }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-           if (preference == mPropOptions) {
+           if (preference == mGamePropsSpoof
+               || preference == mPropOptions) {
               systemUtils.showSystemRestartDialog(getContext());
             return true;
     }
