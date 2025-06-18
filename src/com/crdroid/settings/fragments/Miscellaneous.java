c8f2f8e00ee6f0cde01ef1cf593bb61d5fdf0400
darkphnx <dark.phnx12@gmail.com>
2025-06-18 20:03:13 +0000

    MatrixxOverride: Drop safetynet attestation blocking
    * we have moved to keybox spoofing by default and we have rmeoved the spoofing for software based attestaion completely so this is now useless
    
    Signed-off-by: darkphnx <dark.phnx12@gmail.com>

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -72,7 +72,6 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     private static final String POCKET_JUDGE = "pocket_judge";
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
-    private static final String SYS_PROP_OPTIONS_PI = "persist.sys.pixelprops.pi";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
     private static final String SYS_NETFLIX_SPOOF = "persist.sys.pixelprops.netflix";
     
@@ -85,7 +84,6 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
     private KeyboxDataPreference mKeyboxDataPreference;
 
     private Preference mPocketJudge;
-    private Preference mPropOptionsPi;
 
     private Preference mPifJsonFilePreference;
 
@@ -131,9 +129,6 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         }
     }
     );
-     
-        mPropOptionsPi = (Preference) findPreference(SYS_PROP_OPTIONS_PI);
-        mPropOptionsPi.setOnPreferenceChangeListener(this);
 
         mPocketJudge = (Preference) prefScreen.findPreference(POCKET_JUDGE);
         boolean mPocketJudgeSupported = res.getBoolean(
@@ -288,7 +283,7 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mGamePropsSpoof || preference == mPropOptionsPi) {
+        if (preference == mGamePropsSpoof) {
                     SystemRestartUtils.showSystemRestartDialog(getContext());
             return true;
         }
