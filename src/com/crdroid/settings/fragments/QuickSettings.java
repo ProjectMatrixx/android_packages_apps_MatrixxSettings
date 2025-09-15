b81a3e0647f0dff55b14eb43ea0bb27c763fef08
darkphnx <dark.phnx12@gmail.com>
2025-09-15 04:47:23 +0000

    Matrixx Settings:[DROP] Implement Better QS Switch [3/3]
    Signed-off-by: darkphnx <dark.phnx12@gmail.com>

--- src/com/crdroid/settings/fragments/QuickSettings.java
+++ src/com/crdroid/settings/fragments/QuickSettings.java
@@ -73,13 +73,11 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private ListPreference mTileAnimationInterpolator;
     private ListPreference mQsUI;
     private ListPreference mQsPanelStyle;
-    private Preference mSplitShadePref;
     private Preference  mNotificationStyleSwitch;
     private SecureSettingSwitchPreference mQsRefactorEnabled;
 
     private  static ThemeUtils mThemeUtils;
 
-    private  ThemeUtils mThemeUtils_split;
 
     private  ThemeUtils mThemeUtils_noti;
 
@@ -135,9 +133,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         mQsPanelStyle = (ListPreference) findPreference(KEY_QS_PANEL_STYLE);
         mQsPanelStyle.setOnPreferenceChangeListener(this);
 
-        mSplitShadePref = (Preference) findPreference("qs_split_shade_enabled");
-        mSplitShadePref.setOnPreferenceChangeListener(this);
-
         mNotificationStyleSwitch = (Preference) findPreference("notification_style");
         mNotificationStyleSwitch.setOnPreferenceChangeListener(this);
 
@@ -172,12 +167,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                     Settings.System.QS_PANEL_STYLE, value, UserHandle.USER_CURRENT);
             updateQsPanelStyle(getContext());
             checkQSOverlays(getContext());
-            return true;
-        } else if (preference == mSplitShadePref) {
-            int value = (boolean) newValue ? 1 : 0;
-            Settings.System.putIntForUser(resolver,
-                "qs_split_shade_enabled", value, UserHandle.USER_CURRENT);
-            updateSplitShadeEnabled(getActivity());
             return true;
         } else if (preference == mNotificationStyleSwitch ) {
                 int value = (boolean) newValue ? 1 : 0;
@@ -242,25 +231,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private void updateAnimTileStyle(int tileAnimationStyle) {
         mTileAnimationDuration.setEnabled(tileAnimationStyle != 0);
         mTileAnimationInterpolator.setEnabled(tileAnimationStyle != 0);
-    }
-
-    private void updateSplitShadeEnabled(Context context) {
-        ContentResolver resolver = context.getContentResolver();
-        boolean splitShadeEnabled = Settings.System.getIntForUser(
-                resolver,
-                "qs_split_shade_enabled" , 0, UserHandle.USER_CURRENT) != 0;
-	    String splitShadeStyleCategory = "android.theme.customization.better_qs";
-        String overlayThemeTarget  = "com.android.systemui";
-        String overlayThemePackage  = "com.android.system.qs.ui.better_qs";
-        if (mThemeUtils_split == null) {
-            mThemeUtils_split = new ThemeUtils(context);
-        }
-        mHandler.postDelayed(() -> {
-            mThemeUtils_split.setOverlayEnabled(splitShadeStyleCategory, overlayThemeTarget, overlayThemeTarget);
-            if (splitShadeEnabled) {
-                mThemeUtils_split.setOverlayEnabled(splitShadeStyleCategory, overlayThemePackage, overlayThemeTarget);
-            }
-        }, 1250);
     }
 
     private static void updateQsStyle(Context context) {
