bbe569130a3c0cf6aa109a745e6d89a4d4a80c3b
darkphnx <dark.phnx12@gmail.com>
2025-09-25 12:08:44 +0000

    MatrixxOverride: restore safetynet attestation blocking
    * also make keybox depended on attestation blocking switch
    
    This reverts commit c8f2f8e00ee6f0cde01ef1cf593bb61d5fdf0400.

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -73,6 +73,7 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     private static final String POCKET_JUDGE = "pocket_judge";
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
+    private static final String SYS_PROP_OPTIONS_PI = "persist.sys.pixelprops.pi";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
     private static final String SYS_NETFLIX_SPOOF = "persist.sys.pixelprops.netflix";
     
@@ -87,6 +88,7 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
     private KeyboxDataPreference mKeyboxDataPreference;
 
     private Preference mPocketJudge;
+    private Preference mPropOptionsPi;
 
     private Preference mPifJsonFilePreference;
 
@@ -135,6 +137,9 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         }
     }
     );
+     
+        mPropOptionsPi = (Preference) findPreference(SYS_PROP_OPTIONS_PI);
+        mPropOptionsPi.setOnPreferenceChangeListener(this);
 
         mPocketJudge = (Preference) prefScreen.findPreference(POCKET_JUDGE);
         boolean mPocketJudgeSupported = res.getBoolean(
@@ -296,7 +301,7 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mGamePropsSpoof) {
+        if (preference == mGamePropsSpoof || preference == mPropOptionsPi) {
                     SystemRestartUtils.showSystemRestartDialog(getContext());
             return true;
         }
