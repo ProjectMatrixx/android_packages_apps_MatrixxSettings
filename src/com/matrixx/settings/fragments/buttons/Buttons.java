bf1c8f1c49ac9d38cfdf99fd584109ebb5a15d1e
Pranav Vashi <neobuddy89@gmail.com>
2026-03-23 19:11:30 +0530

    MatrixxSettings: Add Swap capacitive buttons pref

--- src/com/matrixx/settings/fragments/buttons/Buttons.java
+++ src/com/matrixx/settings/fragments/buttons/Buttons.java
@@ -34,6 +34,7 @@ import com.android.settingslib.search.SearchIndexable;
 import java.util.List;
 
 import lineageos.hardware.LineageHardwareManager;
+import lineageos.providers.LineageSettings;
 
 @SearchIndexable
 public class Buttons extends SettingsPreferenceFragment
@@ -42,8 +43,11 @@ public class Buttons extends SettingsPreferenceFragment
     private static final String TAG = "Buttons";
 
     private static final String HWKEYS_DISABLED = "hardware_keys_disable";
+    private static final String KEY_SWAP_CAPACITIVE_KEYS = "swap_capacitive_keys";
 
     private SwitchPreferenceCompat mHardwareKeysDisable;
+    private SwitchPreferenceCompat mSwapCapacitiveKeys;
+
     private LineageHardwareManager mHardware;
 
     @Override
@@ -56,8 +60,8 @@ public class Buttons extends SettingsPreferenceFragment
 
         final ContentResolver resolver = getActivity().getContentResolver();
 
+        // HW Keys Disable
         mHardwareKeysDisable = findPreference(HWKEYS_DISABLED);
-
         if (mHardwareKeysDisable != null) {
             if (isKeyDisablerSupported(getActivity())) {
                 int enabled = Settings.System.getIntForUser(
@@ -71,6 +75,23 @@ public class Buttons extends SettingsPreferenceFragment
                 mHardwareKeysDisable.setOnPreferenceChangeListener(this);
             } else {
                 getPreferenceScreen().removePreference(mHardwareKeysDisable);
+            }
+        }
+
+        // Swap Capacitive Keys
+        mSwapCapacitiveKeys = findPreference(KEY_SWAP_CAPACITIVE_KEYS);
+        if (mSwapCapacitiveKeys != null) {
+            if (isKeySwapperSupported(getActivity())) {
+                int enabled = LineageSettings.System.getInt(
+                        resolver,
+                        LineageSettings.System.KEY_SWAP_CAPACITIVE_KEYS,
+                        0
+                );
+
+                mSwapCapacitiveKeys.setChecked(enabled == 1);
+                mSwapCapacitiveKeys.setOnPreferenceChangeListener(this);
+            } else {
+                getPreferenceScreen().removePreference(mSwapCapacitiveKeys);
             }
         }
     }
@@ -87,7 +108,18 @@ public class Buttons extends SettingsPreferenceFragment
                     UserHandle.USER_CURRENT
             );
             return true;
+
+        } else if (preference == mSwapCapacitiveKeys) {
+            boolean value = (Boolean) newValue;
+
+            LineageSettings.System.putInt(
+                    getContentResolver(),
+                    LineageSettings.System.KEY_SWAP_CAPACITIVE_KEYS,
+                    value ? 1 : 0
+            );
+            return true;
         }
+
         return false;
     }
 
@@ -100,12 +132,24 @@ public class Buttons extends SettingsPreferenceFragment
                 0,
                 UserHandle.USER_CURRENT
         );
+
+        LineageSettings.System.putInt(
+                resolver,
+                LineageSettings.System.KEY_SWAP_CAPACITIVE_KEYS,
+                0
+        );
     }
 
     private static boolean isKeyDisablerSupported(Context context) {
         final LineageHardwareManager hardware =
                 LineageHardwareManager.getInstance(context);
         return hardware.isSupported(LineageHardwareManager.FEATURE_KEY_DISABLE);
+    }
+
+    private static boolean isKeySwapperSupported(Context context) {
+        final LineageHardwareManager hardware =
+                LineageHardwareManager.getInstance(context);
+        return hardware.isSupported(LineageHardwareManager.FEATURE_KEY_SWAP);
     }
 
     @Override
@@ -125,6 +169,10 @@ public class Buttons extends SettingsPreferenceFragment
 
                     if (!isKeyDisablerSupported(context)) {
                         keys.add(HWKEYS_DISABLED);
+                    }
+
+                    if (!isKeySwapperSupported(context)) {
+                        keys.add(KEY_SWAP_CAPACITIVE_KEYS);
                     }
 
                     return keys;
