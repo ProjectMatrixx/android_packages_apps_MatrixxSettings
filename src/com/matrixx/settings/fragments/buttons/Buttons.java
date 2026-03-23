5d25089339b16434544ab0a30bd04c6f870f57b4
Pranav Vashi <neobuddy89@gmail.com>
2026-03-23 15:38:33 +0530

    Add toggle to disable HW keys [2/2] This requires device side implementation. Ref: crdroidandroid/android_device_oneplus_cheeseburger@1eb6a8a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/matrixx/settings/fragments/buttons/Buttons.java
+++ src/com/matrixx/settings/fragments/buttons/Buttons.java
@@ -19,8 +19,11 @@ package com.matrixx.settings.fragments.buttons;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 import androidx.preference.Preference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
@@ -30,27 +33,79 @@ import com.android.settingslib.search.SearchIndexable;
 
 import java.util.List;
 
+import lineageos.hardware.LineageHardwareManager;
+
 @SearchIndexable
 public class Buttons extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "Buttons";
 
+    private static final String HWKEYS_DISABLED = "hardware_keys_disable";
+
+    private SwitchPreferenceCompat mHardwareKeysDisable;
+    private LineageHardwareManager mHardware;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mHardware = LineageHardwareManager.getInstance(getActivity());
+
         addPreferencesFromResource(R.xml.matrixx_settings_button);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mHardwareKeysDisable = findPreference(HWKEYS_DISABLED);
+
+        if (mHardwareKeysDisable != null) {
+            if (isKeyDisablerSupported(getActivity())) {
+                int enabled = Settings.System.getIntForUser(
+                        resolver,
+                        Settings.System.HARDWARE_KEYS_DISABLE,
+                        0,
+                        UserHandle.USER_CURRENT
+                );
+
+                mHardwareKeysDisable.setChecked(enabled == 1);
+                mHardwareKeysDisable.setOnPreferenceChangeListener(this);
+            } else {
+                getPreferenceScreen().removePreference(mHardwareKeysDisable);
+            }
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHardwareKeysDisable) {
+            boolean value = (Boolean) newValue;
+
+            Settings.System.putIntForUser(
+                    getContentResolver(),
+                    Settings.System.HARDWARE_KEYS_DISABLE,
+                    value ? 1 : 0,
+                    UserHandle.USER_CURRENT
+            );
+            return true;
+        }
         return false;
     }
 
     public static void reset(Context context) {
         ContentResolver resolver = context.getContentResolver();
 
+        Settings.System.putIntForUser(
+                resolver,
+                Settings.System.HARDWARE_KEYS_DISABLE,
+                0,
+                UserHandle.USER_CURRENT
+        );
+    }
+
+    private static boolean isKeyDisablerSupported(Context context) {
+        final LineageHardwareManager hardware =
+                LineageHardwareManager.getInstance(context);
+        return hardware.isSupported(LineageHardwareManager.FEATURE_KEY_DISABLE);
     }
 
     @Override
@@ -66,7 +121,13 @@ public class Buttons extends SettingsPreferenceFragment
 
                 @Override
                 public List<String> getNonIndexableKeys(Context context) {
-                    return super.getNonIndexableKeys(context);
+                    List<String> keys = super.getNonIndexableKeys(context);
+
+                    if (!isKeyDisablerSupported(context)) {
+                        keys.add(HWKEYS_DISABLED);
+                    }
+
+                    return keys;
                 }
             };
 }
