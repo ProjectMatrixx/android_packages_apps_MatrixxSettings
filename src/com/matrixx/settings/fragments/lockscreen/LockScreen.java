32d633bb012c2e7c2d1b558a856594d66098bbe6
Pranav Vashi <neobuddy89@gmail.com>
2026-03-25 16:23:33 +0530

    MatrixxSettings: Add Smartspace Toggle & OmniJaws Weather pref

--- src/com/matrixx/settings/fragments/lockscreen/LockScreen.java
+++ src/com/matrixx/settings/fragments/lockscreen/LockScreen.java
@@ -40,8 +40,12 @@ public class LockScreen extends SettingsPreferenceFragment
     public static final String TAG = "LockScreen";
 
     private static final String KEY_KG_USER_SWITCHER= "kg_user_switcher_enabled";
+    private static final String KEY_SMARTSPACE = "lockscreen_smartspace_enabled";
+    private static final String KEY_WEATHER = "lockscreen_weather_enabled";
 
     private Preference mUserSwitcher;
+    private SwitchPreferenceCompat mSmartspace;
+    private SwitchPreferenceCompat mWeather;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,15 +55,46 @@ public class LockScreen extends SettingsPreferenceFragment
 
         mUserSwitcher = (Preference) findPreference(KEY_KG_USER_SWITCHER);
         mUserSwitcher.setOnPreferenceChangeListener(this);
+
+        mSmartspace = (SwitchPreferenceCompat) findPreference(KEY_SMARTSPACE);
+        mSmartspace.setOnPreferenceChangeListener(this);
+
+        mWeather = (SwitchPreferenceCompat) findPreference(KEY_WEATHER);
+        mWeather.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mUserSwitcher) {
-            SystemRestartUtils.showSystemUIRestartDialog(getContext());
+        if (preference == mSmartspace) {
+            mSmartspace.setChecked((Boolean)newValue);
+            updateWeatherSettings();
+            SystemUtils.showSystemUiRestartDialog(getContext());
+            return true;
+        } else if (preference == mWeather) {
+            mWeather.setChecked((Boolean)newValue);
+            SystemUtils.showSystemUiRestartDialog(getContext());
+            return true;
+        } else if (preference == mKgUserSwitcher) {
+            mKgUserSwitcher.setChecked((Boolean)newValue);
+            SystemUtils.showSystemUiRestartDialog(getContext());
             return true;
         }
         return false;
+    }
+
+    private void updateWeatherSettings() {
+        if (mWeather == null || mSmartspace == null) return;
+
+        boolean weatherEnabled = OmniJawsClient.get().isOmniJawsEnabled(getContext());
+        mWeather.setEnabled(!mSmartspace.isChecked() && weatherEnabled);
+        mWeather.setSummary(!mSmartspace.isChecked() && weatherEnabled
+                ? R.string.lockscreen_weather_summary
+                : R.string.lockscreen_weather_enabled_info);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateWeatherSettings();
     }
 
     public static void reset(Context context) {
