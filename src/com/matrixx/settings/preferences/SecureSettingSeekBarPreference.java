3704d3cb4bb2cc7d64b380799eceabdbde034516
Mrick343 <mrinmoy8429de@gmail.com>
2026-03-29 11:13:40 +0530

    MatrixxSettings: Update SeekBarPreference from Lunaris-AOSP  -ref: https://github.com/Lunaris-AOSP/packages_apps_Singularity/blob/16.2/src/org/lunaris/settings/preferences/

--- src/com/matrixx/settings/preferences/SecureSettingSeekBarPreference.java
+++ src/com/matrixx/settings/preferences/SecureSettingSeekBarPreference.java
@@ -20,6 +20,11 @@ import android.util.AttributeSet;
 
 public class SecureSettingSeekBarPreference extends CustomSeekBarPreference {
 
+    public SecureSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
     public SecureSettingSeekBarPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
         setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
